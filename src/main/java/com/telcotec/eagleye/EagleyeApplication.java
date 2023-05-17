cc85c2330e58ab38ddeafd3d1988a827323f0636
abdelkader5 <Abdelkader.belhadj@esprit.tn>
2023-05-17 16:40:25 +0100

    first commit

---
+++ src/main/java/com/telcotec/eagleye/EagleyeApplication.java
@@ -0,0 +1,13 @@
+package com.telcotec.eagleye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EagleyeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/telcotec/eagleye/EagleyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telcotec.eagleye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EagleyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
